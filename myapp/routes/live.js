95ccabc89066d9bff4c0d503d9ddd3e85560fcd4
formation <formation@ubuntu.ubuntu-domain>
2025-02-04 14:33:01 +0100

    Initial commit

---
+++ myapp/routes/live.js
@@ -0,0 +1,94 @@
+var express = require('express')
+var router = express.Router()
+
+repl.repl.ignoreUndefined=true
+
+const {InfluxDB, Point} = require('@influxdata/influxdb-client')
+
+const token = process.env.INFLUXDB_TOKEN
+const url = 'http://localhost:8086'
+
+const client = new InfluxDB({url, token})
+
+let org = `thomas_et_antonin`
+let bucket = `db32`
+
+
+
+let queryClient = client.getQueryApi(org)
+let fluxQuery = `from(bucket: "db32")
+ |> range(start: -10m)
+ |> filter(fn: (r) => r._measurement == "measurement1")`
+
+queryClient.queryRows(fluxQuery, {
+  next: (row, tableMeta) => {
+    const tableObject = tableMeta.toObject(row)
+    console.log(tableObject)
+  },
+  error: (error) => {
+    console.error('\nError', error)
+  },
+  complete: () => {
+    console.log('\nSuccess')
+  },
+})
+
+
+
+
+/**
+router.get('/init', function(req, res, next) {
+  MongoClient.connect(url, function(err, client) {
+    if (err) {throw err}
+    console.log("Connected successfully to server")
+    const db = client.db(dbName)
+    db.collection('restaurants').createIndex({ "location" : "2dsphere" }).then((result)=>{
+      res.json("Index " + result + " created")
+      client.close()
+    })
+  })
+})
+
+
+router.get('/', function(req, res, next) {
+  console.log("test / router");
+  MongoClient.connect(url, function(err, client) {
+    console.log("Connected successfully to server");
+    if (err) {throw err}
+    console.log("Connected successfully to server")
+    const db = client.db(dbName)
+    db.collection('restaurants').find().toArray(function(err, result) {
+      if (err) {throw err}
+      res.json(result)
+      client.close()
+    })
+  })
+  
+})
+
+
+
+
+router.get('/intersect/:bbox', function(req, res, next) {
+  MongoClient.connect(url, function(err, client) {
+    if (err) {throw err}
+
+    let [y1,x1,y2,x2] = req.params.bbox.split(",").map(n=>Number(n))
+
+    let find = { location: { $geoWithin: { $box: [ [ y1,x1 ],[ y2,x2 ] ] } } }
+
+    console.log("look for this bbox : ",find)
+    const db = client.db(dbName);
+
+    db.collection('restaurants').find(find).toArray(function(err, result) {
+      if (err) {throw err}
+      res.json(result)
+      client.close()
+    })
+  })
+})
+
+**/
+
+
+module.exports = router
