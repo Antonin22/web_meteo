49cc1aa3876d49aee7df7612dd4dac14a3eb89bc
BoThoma <thomas.boutonne@ensg.eu>
2025-02-27 17:47:21 +0100

    JE SAIS PAS LIRE LA DOC

--- myapp/app.js
+++ myapp/app.js
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/live', liveRouter);
 app.use('/sample', sampleRouter);
-app.use(cors);
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
