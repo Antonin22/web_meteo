ceb44b1781da98761c986dc9c6d2f63237ef00ef
BoThoma <thomas.boutonne@ensg.eu>
2025-02-27 17:54:00 +0100

    au dessus

--- myapp/app.js
+++ myapp/app.js
@@ -22,11 +22,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/live', liveRouter);
 app.use('/sample', sampleRouter);
-app.use(cors());
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
